adf247d18dc567ed3b53cc41cefa74e0088f64bf
LenaYork <lenayork@tut.by>
2021-11-21 22:49:01 +0300

    feat: add html structure

--- src/components/Main/Buttons/Buttons.js
+++ src/components/Main/Buttons/Buttons.js
@@ -3,6 +3,12 @@ import "./Buttons.css";
 
 export function Buttons() {
     return(
-        <div></div>
+        <div className="buttons-container">
+            <input type="text" className="total-counter" placeholder="0" title="total" disabled />
+            <button type="button" className="done-button">Done</button>
+            <button type="button" className="pending-button">Pending</button>
+            <button type="button" className="all-button">All</button>
+            <button type="button" className="delete-button">Del</button>
+        </div>
     )
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Input} from './Input';
 import { List } from './List';
+import { Buttons } from './Buttons';
+import './Main.css';
 
 export function Main() {
     return( 
         <div>
             <Input />
             <List />
+            <Buttons />
         </div>
     )
 }
