97178ccdb10a1117da5c3d923dc6ab9c7eb9f4dc
LenaYork <lenayork@tut.by>
2021-11-26 20:07:55 +0300

    add Button component

--- src/components/Main/Buttons/Buttons.js
+++ src/components/Main/Buttons/Buttons.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Button } from "../../common/Button";
 import "./Buttons.css";
 
 export function Buttons() {
     return(
         <div className="buttons-container">
             <input type="text" className="total-counter" placeholder="0" title="total" disabled />
-            <button type="button" className="done-button">Done</button>
-            <button type="button" className="pending-button">Pending</button>
-            <button type="button" className="all-button">All</button>
-            <button type="button" className="delete-button">Del</button>
+            <Button className="green-button" text="Done"/>
+            <Button className="green-button" text="Pedning"/>
+            <Button className="red-button" text="All"/>
         </div>
     )
 }
---
+++ src/components/common/Button/Button.js
@@ -0,0 +1,9 @@
+import "./Button.css";
+
+export function Button(props) {
+    return(
+        <button className={props.className}>
+            {props.text} 
+        </button> 
+    )
+}
